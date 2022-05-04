f3c86299eb2d8d45e6011686996c64eb94ba18f5
Cristhian Carmona <cristhian1016l@gmail.com>
2022-05-04 11:42:25 -0500

    CAMBIOS EN LOS REPORTES
    REalizando los cambios en los reportes de visitas de casas de paz

--- app/Http/Controllers/Liderred/ReportsController.php
+++ app/Http/Controllers/Liderred/ReportsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Liderred;
 
 use App\Http\Controllers\Controller;
 use App\Tabredes;
+use App\Tabasi;
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade as PDF;
 use Illuminate\Support\Facades\Auth;
@@ -17,21 +18,41 @@ class ReportsController extends Controller
     }
 
     public function FaultsOfMemberDownload(Request $request){
+        // 070420220838
         $datosRed = Tabredes::select('ID_RED')->where('LID_RED', Auth::user()->codcon)->first();        
         $id_red = $datosRed->ID_RED;
         $datos = array();
+        $fecCulto = Tabasi::select('FecAsi')->where('CodAct', '001')->OrderBy('FecAsi', 'desc')->first();
         $cdps = DB::select("SELECT cdp.CodCasPaz, c.ApeCon, c.NomCon FROM TabCasasDePaz cdp INNER JOIN TabCon c 
-                            ON cdp.CodLid = c.CodCon WHERE cdp.ID_Red = '".$id_red."' ORDER BY cdp.CodCasPaz");
+                            ON cdp.CodLid = c.CodCon WHERE cdp.ID_Red = '".$id_red."' ORDER BY cdp.CodCasPaz LIMIT 2");
+        // dd($cdps);
         foreach($cdps as $cdp){
-            $cdps = DB::select("SELECT da.NomApeCon, da.EstAsi, c.NumCel FROM TabDetAsi da
-                            INNER JOIN TabMimCasPaz mcdp ON da.CodCon = mcdp.CodCon
-                            INNER JOIN TabCon c ON mcdp.CodCon = c.CodCon
-                            WHERE da.CodAsi = '".$request->culto."' AND mcdp.CodCasPaz = '".$cdp->CodCasPaz."' AND da.EstAsi = 'F' ORDER BY da.NomApeCon");
-            array_push($datos, ["cdp" => $cdp->CodCasPaz, "lider" => $cdp->ApeCon.' '.$cdp->NomCon, "members" => $cdps]);
-        }
+            // $cdps = DB::select("SELECT da.NomApeCon, da.EstAsi, c.NumCel FROM TabDetAsi da
+            //                 INNER JOIN TabMimCasPaz mcdp ON da.CodCon = mcdp.CodCon
+            //                 INNER JOIN TabCon c ON mcdp.CodCon = c.CodCon
+            //                 WHERE da.CodAsi = '".$request->culto."' AND mcdp.CodCasPaz = '".$cdp->CodCasPaz."' AND da.EstAsi = 'F' ORDER BY da.NomApeCon");
 
-        // dd($datos);
-        // $request->culto
+            $members = DB::select("SELECT c.CodCon, c.TipCon, c.SoloCasPaz FROM TabMimCasPaz mcdp INNER JOIN TabCon c ON mcdp.CodCon = c.CodCon
+                                    WHERE mcdp.CodCasPaz = '".$cdp->CodCasPaz."'");
+
+            dd($members);
+
+            foreach($members as $member){
+                $asistenciaCulto = DB::select("SELECT da.NomApeCon, da.Asistio, da.EstAsi FROM TabAsi a INNER JOIN TabDetAsi da ON a.CodAsi = da.CodAsi 
+                                            WHERE a.FecAsi = '".$fecCulto->FecAsi."' AND da.CodCon = '".$member->CodCon."' AND CodAct = '001'");
+                
+            }
+
+
+            // dd($members);
+            // $membersAssistance = DB::select("SELECT da.NomApeCon, da.EstAsi, c.NumCel FROM TabDetAsi da
+            //                 INNER JOIN TabMimCasPaz mcdp ON da.CodCon = mcdp.CodCon
+            //                 INNER JOIN TabCon c ON mcdp.CodCon = c.CodCon
+            //                 WHERE da.CodAsi = '".$request->culto."' AND mcdp.CodCasPaz = '".$cdp->CodCasPaz."' AND da.EstAsi = 'F' ORDER BY da.NomApeCon");
+            dd($membersAssistance);
+            array_push($datos, ["cdp" => $cdp->CodCasPaz, "lider" => $cdp->ApeCon.' '.$cdp->NomCon, "members" => $membersAssistance]);
+        }
+        
         $data = ['cdps' => $datos];
         $pdf=PDF::loadView('liderred.reportes_cdp.faultToCults', $data);
         return $pdf->stream();
