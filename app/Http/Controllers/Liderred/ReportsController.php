4c22ee685255ee97779e3924a137a5f36ac5c4b8
Cristhian Carmona <cristhian1016l@gmail.com>
2022-05-04 12:57:24 -0500

    Cambios

--- app/Http/Controllers/Liderred/ReportsController.php
+++ app/Http/Controllers/Liderred/ReportsController.php
@@ -25,6 +25,8 @@ class ReportsController extends Controller
         $fecCulto = Tabasi::select('FecAsi')->where('CodAct', '001')->OrderBy('FecAsi', 'desc')->first();
         $cdps = DB::select("SELECT cdp.CodCasPaz, c.ApeCon, c.NomCon FROM TabCasasDePaz cdp INNER JOIN TabCon c 
                             ON cdp.CodLid = c.CodCon WHERE cdp.ID_Red = '".$id_red."' ORDER BY cdp.CodCasPaz LIMIT 2");
+
+        $miembros = array();
         // dd($cdps);
         foreach($cdps as $cdp){
             // $cdps = DB::select("SELECT da.NomApeCon, da.EstAsi, c.NumCel FROM TabDetAsi da
@@ -35,22 +37,19 @@ class ReportsController extends Controller
             $members = DB::select("SELECT c.CodCon, c.TipCon, c.SoloCasPaz FROM TabMimCasPaz mcdp INNER JOIN TabCon c ON mcdp.CodCon = c.CodCon
                                     WHERE mcdp.CodCasPaz = '".$cdp->CodCasPaz."'");
 
-            dd($members);
-
             foreach($members as $member){
                 $asistenciaCulto = DB::select("SELECT da.NomApeCon, da.Asistio, da.EstAsi FROM TabAsi a INNER JOIN TabDetAsi da ON a.CodAsi = da.CodAsi 
-                                            WHERE a.FecAsi = '".$fecCulto->FecAsi."' AND da.CodCon = '".$member->CodCon."' AND CodAct = '001'");
+                                            WHERE a.FecAsi = '".$fecCulto->FecAsi."' AND da.CodCon = '".$member->CodCon."' AND CodAct = '001'");                
                 
+                for($i = 1; $i < count($asistenciaCulto); $i++){
+                    if($asistenciaCulto[$i]->EstAsi === "F"){
+                        array_push($miembros, ["miembro" => $member, "asistencia" => $asistenciaCulto]);   
+                    }
+                }
             }
 
-
-            // dd($members);
-            // $membersAssistance = DB::select("SELECT da.NomApeCon, da.EstAsi, c.NumCel FROM TabDetAsi da
-            //                 INNER JOIN TabMimCasPaz mcdp ON da.CodCon = mcdp.CodCon
-            //                 INNER JOIN TabCon c ON mcdp.CodCon = c.CodCon
-            //                 WHERE da.CodAsi = '".$request->culto."' AND mcdp.CodCasPaz = '".$cdp->CodCasPaz."' AND da.EstAsi = 'F' ORDER BY da.NomApeCon");
-            dd($membersAssistance);
-            array_push($datos, ["cdp" => $cdp->CodCasPaz, "lider" => $cdp->ApeCon.' '.$cdp->NomCon, "members" => $membersAssistance]);
+            dd($miembros);
+            array_push($datos, ["cdp" => $cdp->CodCasPaz, "lider" => $cdp->ApeCon.' '.$cdp->NomCon, "members" => $miembros]);
         }
         
         $data = ['cdps' => $datos];
