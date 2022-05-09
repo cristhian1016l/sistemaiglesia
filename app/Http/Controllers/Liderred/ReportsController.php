1c3e797f35ece56eb058e70def7f840113ea1667
Cristhian Carmona <cristhian1016l@gmail.com>
2022-05-09 16:56:34 -0500

    fi

--- app/Http/Controllers/Liderred/ReportsController.php
+++ app/Http/Controllers/Liderred/ReportsController.php
@@ -25,7 +25,7 @@ class ReportsController extends Controller
         // $fecCulto = Tabasi::select('FecAsi')->where('CodAct', '001')->OrderBy('FecAsi', 'desc')->first();
         $fecCulto = $request->culto;
         $cdps = DB::select("SELECT cdp.CodCasPaz, c.ApeCon, c.NomCon FROM TabCasasDePaz cdp INNER JOIN TabCon c 
-                            ON cdp.CodLid = c.CodCon WHERE cdp.ID_Red = '".$id_red."' ORDER BY cdp.CodCasPaz LIMIT 6");
+                            ON cdp.CodLid = c.CodCon WHERE cdp.ID_Red = '".$id_red."' ORDER BY cdp.CodCasPaz");
         
         $miembros = array();
         // dd($cdps);
