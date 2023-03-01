da52f73c3206f17d5a58ec10899c68d40609cca1
Cristhian Carmona <cristhian1016l@gmail.com>
2023-02-28 21:37:43 -0500

    changes

--- app/Http/Controllers/Admin/AssistanceController.php
+++ app/Http/Controllers/Admin/AssistanceController.php
@@ -267,9 +267,10 @@ class AssistanceController extends Controller
     public function imprimirReporteDeOracion()
     {
         $discipulos = DB::select("SELECT t.CodArea, t.CodCon, t.NomCon, t.ApeCon, t.Lunes, t.Martes, t.Miercoles, t.Jueves, t.Viernes,
-                                t.Sabado, t.Domingo, t.NumAsi, t.TotAsi, CONCAT(c.ApeCon, ' ', c.NomCon) AS Encargado FROM TabTempOracion t INNER JOIN
-                                TabCon c ON t.CodCon = c.CodCon ORDER BY t.CodArea, t.ApeCon ASC");
-        $discipulados = DB::select("SELECT * FROM TabGrupos WHERE TipGrup = 'D'");
+                                t.Sabado, t.Domingo, t.NumAsi, t.TotAsi, g.DesArea, g.EncArea, g.TipGrup FROM TabTempOracion t INNER JOIN
+                                TabGrupos g ON t.CodArea = g.CodArea ORDER BY t.CodArea, t.ApeCon ASC");
+        $discipulados = DB::select("SELECT g.CodArea, CONCAT(c.ApeCon, ' ', c.NomCon) as Encargado FROM
+                                    TabGrupos g INNER JOIN TabCon c ON g.CodCon = c.CodCon WHERE g.TipGrup = 'D'");
         $data = ['discipulos' => $discipulos, 'discipulados' => $discipulados];
         $pdf=PDF::loadView('admin.reports.oracion_discipulados', $data);
         return $pdf->stream();
