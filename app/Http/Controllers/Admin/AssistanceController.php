2bb090f302e715712c42215a0eee44d4749a1188
Cristhian Carmona <cristhian1016l@gmail.com>
2023-02-28 21:47:41 -0500

    changes

--- app/Http/Controllers/Admin/AssistanceController.php
+++ app/Http/Controllers/Admin/AssistanceController.php
@@ -266,9 +266,8 @@ class AssistanceController extends Controller
 
     public function imprimirReporteDeOracion()
     {
-        $discipulos = DB::select("SELECT t.CodArea, t.CodCon, t.NomCon, t.ApeCon, t.Lunes, t.Martes, t.Miercoles, t.Jueves, t.Viernes,
-                                t.Sabado, t.Domingo, t.NumAsi, t.TotAsi, g.DesArea, g.TipGrup FROM TabTempOracion t INNER JOIN
-                                TabGrupos g ON t.CodArea = g.CodArea ORDER BY t.CodArea, t.ApeCon ASC");
+        $discipulos = DB::select("SELECT CodArea, CodCon, NomCon, ApeCon, Lunes, Martes, Miercoles, Jueves, Viernes,
+                                Sabado, Domingo, NumAsi, TotAsi FROM TabTempOracion ORDER BY CodArea, ApeCon ASC");
         $discipulados = DB::select("SELECT g.CodArea, CONCAT(c.ApeCon, ' ', c.NomCon) as Encargado FROM
                                     TabGrupos g INNER JOIN TabCon c ON g.CodCon = c.CodCon WHERE g.TipGrup = 'D'");
         $data = ['discipulos' => $discipulos, 'discipulados' => $discipulados];
